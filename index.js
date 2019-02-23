80cbac87465dd290315287a01ac961c4d085a3e0
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-23 17:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("#nested .target");
 }
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  var n = document.querySelector("div #grand-node");
+  //while(1) {
+  //  if (n.children.length === 0)  break;
+  //}
+}
